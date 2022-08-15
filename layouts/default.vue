f30b9fda72c3f199b57a421a33c48b55a1ae2dcc
sajibsd013 <sajibsd013@gmail.com>
2022-08-15 13:37:04 +0600

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="root">
+    <Header />
+    <Nuxt />
+    <Footer />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.root {
+  position: relative;
+  min-height: 100vh;
+}
+Footer {
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+}
+</style>
---
+++ plugins/vee-validate.js
@@ -0,0 +1,42 @@
+import { extend } from "vee-validate";
+import { alpha, confirmed, email, length, min, numeric, required } from "vee-validate/dist/rules";
+
+extend("required", {
+  ...required,
+  message: "This field is required"
+});
+
+extend("numeric", {
+  ...numeric,
+  message: "This Value must be numeric"
+});
+
+
+extend("alpha", {
+  ...alpha,
+  message: "This field must only contain alphabetic characters"
+});
+
+extend("email", {
+  ...email,
+  message: "This field must be a valid email"
+});
+
+extend("confirmed", {
+  ...confirmed,
+  message: "This field confirmation does not match"
+});
+
+extend("length", {
+  ...length,
+  message: "This field must have 2 options"
+});
+
+extend("min", {
+  ...min,
+  message: "The field must be at least {length} characters"
+});
+
+
+
+
